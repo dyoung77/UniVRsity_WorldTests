9dd1e4772b87d3ed7a4585c816613da5e8f43c4c
Debbie Young <sweetdreamsstudiosinc@gmail.com>
2020-11-02 20:08:40 -0500

    lighting

--- WILDTIME_Carnival/Assets/Plugins/upload.bat
+++ WILDTIME_Carnival/Assets/Plugins/upload.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\debor\Documents\UNITY_PROJECTS\DY_AltspaceWorld_01\WILDTIME_Carnival\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1595957036753879329.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\debor\Documents\GitHub\UniVRsity_WorldTests\WILDTIME_Carnival\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1595957036753879329.json
